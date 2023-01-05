c93ae70f6e9fbac9b012fbfd71b5164ff1aacc3d
kkjjhh4171@naver.com <kkjjhh4171@naver.com>
2023-01-05 18:12:42 +0900

    Create React Project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends Component {
+  render(){
   return (
     <div className="App">
       <header className="App-header">
@@ -21,5 +22,7 @@ function App() {
     </div>
   );
 }
+}
+
 
 export default App;
