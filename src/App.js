7b8fa2cf690f3fb44d09d746200a2ecc8a9528a1
kkjjhh4171@naver.com <kkjjhh4171@naver.com>
2023-01-06 09:23:56 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
 import logo from './logo.svg';
+import Customer from './components/Customer'
 import './App.css';
+
+
+// 보내는 데이터 명시
+const customers = [
+{
+  'id' : 1,
+  'image' : 'https://placimg.com./64/64/any',
+  'name': '가나다',
+  'birth' : '990101',
+  'gender' : '남자',
+  'job' : '대학생'
+
+},
+
+{
+  'id' : 2,
+  'image' : 'https://placimg.com./64/64/any',
+  'name': '김대한',
+  'birth' : '010504',
+  'gender' : '남자',
+  'job' : '대학생'
+},
+
+{
+  'id' : 3,
+  'image' : 'https://placimg.com./64/64/any',
+  'name': '김민국',
+  'birth' : '980504',
+  'gender' : '남자',
+  'job' : '대학생'
+}
+]
+
 
 class App extends Component {
   render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // props를 통한 데이터 전달 
+    // map형태로 반복, 각 원소를 구분하는 key값 (id값이 다르기 때문에 c.id로 설정)
+    <div>
+      {
+        customers.map(c => {
+          return(
+            <Customer 
+            key={c.id}
+            id={c.id}
+            image={c.image}
+            name={c.name}
+            birth={c.birth}
+            gender={c.gender}
+            job={c.job}
+
+            />
+          );
+
+        })
+      }
+   </div>
   );
 }
 }
