463356c467c6985ccf194986ad807ac66f4df001
kkjjhh4171@naver.com <kkjjhh4171@naver.com>
2023-01-06 13:05:22 +0900

    Apply Material UI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,30 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import Customer from './components/Customer'
 import './App.css';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import { withStyles } from '@material-ui/core/styles';
+
+// styles 변수, 너비100, 마진 3
+// x축으로 오버플로우가 발생할 수 있도록 처리, 가로 스크롤바가 생김
+const styles = theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit *3,
+    overflowX: "auto"
+  },
+
+// 화면 크기가 줄어들어도 전체 1080픽셀은 무조건 테이블 크기가 자리잡음
+  table: {
+    minWidth: 1000
+  }
+})
+
 
 
 // 보내는 데이터 명시
@@ -10,8 +34,8 @@ const customers = [
   'image' : 'https://placimg.com./64/64/any',
   'name': '가나다',
   'birth' : '990101',
-  'gender' : '남자',
-  'job' : '대학생'
+  'gender' : '여자',
+  'job' : '서버 관리자'
 
 },
 
@@ -21,29 +45,43 @@ const customers = [
   'name': '김대한',
   'birth' : '010504',
   'gender' : '남자',
-  'job' : '대학생'
+  'job' : '개발자'
 },
 
 {
   'id' : 3,
   'image' : 'https://placimg.com./64/64/any',
-  'name': '김민국',
+  'name': '박민국',
   'birth' : '980504',
   'gender' : '남자',
   'job' : '대학생'
 }
 ]
 
-
+// Paper는 컴포넌트 외부를 감싸는 컴포넌트
 class App extends Component {
   render(){
+    // 클래스 변수 선언 
+    const {classes} = this.props;
   return (
-    // props를 통한 데이터 전달 
+    // material ui를 이용한 테이블 구조
     // map형태로 반복, 각 원소를 구분하는 key값 (id값이 다르기 때문에 c.id로 설정)
-    <div>
-      {
-        customers.map(c => {
-          return(
+    <Paper className={classes.root}>
+      <Table className={classes.table}>
+        <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+        </TableHead>
+
+        <TableBody>
+            { customers.map(c => { 
+            return ( 
             <Customer 
             key={c.id}
             id={c.id}
@@ -52,16 +90,17 @@ class App extends Component {
             birth={c.birth}
             gender={c.gender}
             job={c.job}
-
             />
-          );
+            );
+            })
+          }
+        </TableBody>
 
-        })
+      </Table>
+    </Paper>
+            
+            );
+        }
       }
-   </div>
-  );
-}
-}
 
-
-export default App;
+export default withStyles(styles)(App);
--- src/components/Customer.js
+++ src/components/Customer.js
@@ -1,4 +1,6 @@
 import React from 'react'; // 특정 라이브러리 불러오기 
+import TableRow from '@material-ui/core/TableRow';  //테이블 행
+import TableCell from '@material-ui/core/TableCell'; //테이블 원소
 
 
 // 일종의 라이브러리이자 클래스인 React.Component
@@ -7,40 +9,22 @@ class Customer extends React.Component{
     // 리액트.컴포넌트가 실제로 그려지는 부분 render, return 형식 필요함
     render() {  
         return(
-            // App.js에서 전달받은 props
-        <div>
-           <CustomerProfile id = {this.props.id} image={this.props.image} name={this.props.name}/>
-           <CustomerInfo birth={this.props.birth} gender={this.props.gender} job={this.props.gender}/>
-        </div>
+            <TableRow>
+            <TableCell>{this.props.id}</TableCell>
+            <TableCell> <img src = {this.props.image} alt="profile"/></TableCell>
+            <TableCell>{this.props.name}</TableCell>
+            <TableCell>{this.props.birth}</TableCell>
+            <TableCell>{this.props.gender}</TableCell>
+            <TableCell>{this.props.job}</TableCell>
+        </TableRow>
         )
     }
 }
 
 // 사용자의 image, id를 랜더링하는 부분 
-class CustomerProfile extends React.Component{
-    render(){
-        return (
-            <div>
-                <img src ={this.props.image} alt = "profile"/>
-                <h2>{this.props.name}({this.props.id})</h2>
-            </div>
-        )
-    }
-}
 
 // 고객의 남은 정보를 출력하는 부분 
-class CustomerInfo extends React.Component{
-    render(){
-        return(
-            <div>
-                <p>{this.props.birth}</p>
-                <p>{this.props.gender}</p>
-                <p>{this.props.jop}</p>
-            </div>
-        )
-    }
 
-}
 
 
 export default Customer; // 다른 컴포넌트에서 사용할 수 있게 내보냄 
