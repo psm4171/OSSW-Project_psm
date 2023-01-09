7756f9f24e2b4a85482efbad6fe299b102af41cd
kkjjhh4171@naver.com <kkjjhh4171@naver.com>
2023-01-09 21:23:04 +0900

    Create Customer DB Table & Node.js DB Connection

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express'); 
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,41 +7,31 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded( { extended: true}));
 
-// 클라이언트가 customers에 접속을 하면, 데이터를 반환 
-// App.js에 명시해놓았던 고객 데이터를 반환 
+// database.json에서 파일을 읽어옴 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data); 
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+
+  host: conf.host,
+  user: conf.user,
+  password: conf.password,
+  port: conf.port,
+  database: conf.database 
+});
+connection.connect();
+
+// 클라이언트가 customers에 접속을 하면, DB에 접근해서 쿼리
+// 가져온 데이터를 rows변수로 처리
+// 모든 고객 데이터가 포함된 rows 변수를 사용자에게 보여줌
 app.get('/api/customers', (req, res) => {
-    res.send([
-
-        {
-            'id' : 1,
-            'image' : 'https://placimg.com./64/64/any',
-            'name': '김나라',
-            'birth' : '990101',
-            'gender' : '여자',
-            'job' : '서버 관리자'
-          
-          },
-          
-          {
-            'id' : 2,
-            'image' : 'https://placimg.com./64/64/any',
-            'name': '김대한',
-            'birth' : '971225',
-            'gender' : '남자',
-            'job' : '개발자'
-          },
-          
-          {
-            'id' : 3,
-            'image' : 'https://placimg.com./64/64/any',
-            'name': '박민국',
-            'birth' : '980504',
-            'gender' : '남자',
-            'job' : '대학생'
-          }
-
-
-        ]);
+    connection.query(
+      "SELECT *FROM CUSTOMER_osswp",
+      (err, rows, field) => {
+        res.send(rows);
+      }
+    );
 
 });
 
