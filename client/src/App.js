afe05f3a32a2c0d0fae513b9fdb76f22409cc317
kkjjhh4171@naver.com <kkjjhh4171@naver.com>
2023-01-17 02:28:48 +0900

    Material UI for Detail App Bar

--- client/src/App.js
+++ client/src/App.js
@@ -1,139 +1,5 @@
 
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import Customer from './components/Customer';
-import CustomerAdd from './components/CustomerAdd';
-import './App.css';
-import Paper from '@material-ui/core/Paper';
-import Table from '@material-ui/core/Table';
-import TableHead from '@material-ui/core/TableHead';
-import TableBody from '@material-ui/core/TableBody';
-import TableRow from '@material-ui/core/TableRow';
-import TableCell from '@material-ui/core/TableCell';
-import  CircularProgress from '@material-ui/core/CircularProgress';
-import { withStyles } from '@material-ui/core/styles';
-
-
-// styles 변수, 너비100, 마진 3
-// x축으로 오버플로우가 발생할 수 있도록 처리, 가로 스크롤바가 생김
-const styles = theme => ({
-  root: {
-    width: '100%',
-    marginTop: theme.spacing.unit *3,
-    overflowX: "auto"
-  },
-
-// 화면 크기가 줄어들어도 전체 1080픽셀은 무조건 테이블 크기가 자리잡음
-  table: {
-    minWidth: 1000
-  },
-
-  progress: {
-    margin: theme.spacing.unit *2
-  }
-
-});
-
-// props or state 변경된 경우 함수 호출, shouldComponentUpdate()
-// 상태 관리를 통해 상태 변환시 화면을 재구성 
-// 고객 정보는 변할 수 있는 데이터, 필요할때마다 서버에서 데이터를 불러옴 
-
-// Paper는 컴포넌트 외부를 감싸는 컴포넌트
-class App extends Component {
-
-  //props는 변할 수 없는 데이터 
-  // state는 컴포넌트 내에서 변할 수 있는 데이터를 가져올 때 
-  // progress 바는 0%에서 100%까지 로딩 화면을 보이기 때문에 complted: 0
-  constructor(props){
-    super(props);
-    this.state = {
-      customers: '',
-      completed: 0
-    }
-  }
-
-  stateRefresh = () => {
-    this.setState({
-      customers: '',
-      completed: 0
-    });
-    this.callApi()
-    .then(res => this.setState({customers: res}))
-    .catch(err => console.log(err));
-
-  }
-
-  // 모든 컴포넌트가 마운트, 고객 목록 데이터를 부르는 함수  
-  componentDidMount(){
-    this.timer = setInterval(this.progress, 20);
-    this.callApi()
-    .then(res => this.setState({customers: res}))
-    .catch(err => console.log(err));
-
-  }
-
-  // 비동기적으로 async 호출 , 고객 목록 데이터를 반환 
-  // 비동기 호출로, api 서버에서 응답을 하지 않으면 로딩 화면만 출력 
-  callApi = async () => {
-    const response = await fetch('/api/customers');
-    const body = await response.json(); 
-    return body;
-  }
-
-  // 0부터 100까지의 값을 왔다갔다 로딩 표시
-  progress = () => {
-    const {completed} = this.state; 
-    this.setState({ completed: completed >= 100 ? 0 : completed + 1});
-  }
-
-  render(){
-    // material ui를 이용한 테이블 구조
-    // map형태로 반복, 각 원소를 구분하는 key값 (id값이 다르기 때문에 c.id로 설정)
-    const {classes} = this.props;
-  return (
-    <div>
-      <Paper className={classes.root}>
-        <Table className={classes.table}>
-          <TableHead>
-              <TableRow>
-                <TableCell>번호</TableCell>
-                <TableCell>이미지</TableCell>
-                <TableCell>이름</TableCell>
-                <TableCell>생년월일</TableCell>
-                <TableCell>성별</TableCell>
-                <TableCell>직업</TableCell>
-                <TableCell>설정</TableCell>
-              </TableRow>
-          </TableHead>
-
-          <TableBody>
-            
-              { this.state.customers ? this.state.customers.map(c => { 
-              return ( <Customer stateRefresh={this.stateRefresh} key={c.id} id={c.id} image={c.image} name={c.name} birth={c.birth} gender={c.gender} job={c.job}/>);
-              }) : 
-              
-              <TableRow>
-              <TableCell colSpan="6" align="center">
-                <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed}/>
-              </TableCell>
-              </TableRow>
-              }
-
-          </TableBody>
-        </Table>
-      </Paper>
-    <CustomerAdd stateRefresh={this.stateRefresh}/>
-  </div>
-    );
-  }
-}
-
-export default withStyles(styles)(App);
-
-
-
-/*
-import React, { Component } from 'react';
 import Customer from './components/Customer'
 import './App.css';
 import Paper from '@material-ui/core/Paper';
@@ -153,18 +19,20 @@ import Typography from '@material-ui/core/Typography';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
-
 const styles = theme => ({
   root: {
     width: "100%",
     minWidth: 1080
   },
+
+  // 고객 추가하기 버튼 : 가운데 정렬 
   menu: {
     marginTop: 15,
     marginBottom: 15,
     display: 'flex',
     justifyContent: 'center'
   },
+
   paper: {
     marginLeft: 18,
     marginRight: 18
@@ -231,8 +99,14 @@ const styles = theme => ({
   }
 });
 
+// 상태 관리를 통해 상태 변환시 화면을 재구성 
+// 고객 정보는 변할 수 있는 데이터, 필요할때마다 서버에서 데이터를 불러옴 
+
 class App extends Component {
 
+   //props는 변할 수 없는 데이터 
+  // state는 컴포넌트 내에서 변할 수 있는 데이터를 가져올 때 
+  // progress 바는 0%에서 100%까지 로딩 화면을 보이기 때문에 complted: 0
   constructor(props) {
     super(props);
     this.state = {
@@ -244,6 +118,7 @@ class App extends Component {
     this.handleValueChange = this.handleValueChange.bind(this)
   }
 
+  // 자동 새로고침 함수
   stateRefresh() {
     this.setState({
       customers: '',
@@ -255,34 +130,37 @@ class App extends Component {
       .catch(err => console.log(err));
   }
 
+  // 모든 컴포넌트가 마운트, 고객 목록 데이터를 부르는 함수
   componentDidMount() {
     this.timer = setInterval(this.progress, 20);
     this.callApi()
       .then(res => this.setState({customers: res}))
       .catch(err => console.log(err));
   }
-
   componentWillUnmount() {
     clearInterval(this.timer);
   }
 
+  // 비동기적으로 async 호출 , 고객 목록 데이터를 반환 
+  // 비동기 호출로, api 서버에서 응답을 하지 않으면 로딩 화면만 출력 
   callApi = async () => {
     const response = await fetch('/api/customers');
     const body = await response.json();
     return body;
   }
 
+  // progress 바는 0%에서 100%까지 로딩 화면을 보이기 때문에 complted: 0
   progress = () => {
     const { completed } = this.state;
     this.setState({ completed: completed >= 100 ? 0 : completed + 1 });
   };
 
+  // 고객 데이터의 다음 정보를 저장하는 함수 
   handleValueChange(e) {
     let nextState = {};
     nextState[e.target.name] = e.target.value;
     this.setState(nextState);
   }
-
   render() {
     const filteredComponents = (data) => {
       data = data.filter((c) => {
@@ -350,7 +228,4 @@ class App extends Component {
     );
   }
 }
-
 export default withStyles(styles)(App);
-
-*/
